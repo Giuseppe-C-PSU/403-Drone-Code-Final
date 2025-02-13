0209136a16291ebe4ccb6559068c0300b221cb1d
Giuseppe Carbone <Giuseppe687.gc@gmail.com>
2025-02-13 11:37:09 -0500

    First commit that works

---
+++ src/main.cpp
@@ -0,0 +1,73 @@
+#include <Arduino.h>
+
+
+#include "../include/rc_pilot_reading.h"
+#include "../include/motors.h"
+#include "../include/rc_pilot.h"
+
+Motors motors;
+extern RC_PILOT rc;
+
+
+uint16_t pwm[4] = {MIN_PWM_OUT,MIN_PWM_OUT,MIN_PWM_OUT,MIN_PWM_OUT};
+uint16_t MotorDataGCS[4] = {MIN_PWM_OUT,MIN_PWM_OUT,MIN_PWM_OUT,MIN_PWM_OUT};
+
+void setup()
+{
+    // controller_setup();
+
+    rc_setup();
+
+    // pozyx_setup();
+
+    motors.init();
+
+    // thermal_setup();
+
+    pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+
+void loop()
+{
+
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+
+    rc_reciever_loop();
+    // delay(1000);
+
+    if(rc.rc_in.AUX2 > 1500)
+    {
+      // pwm[0] = rc.rc_in.THR;
+      // pwm[1] = rc.rc_in.THR;
+      // pwm[2] = rc.rc_in.THR;
+      // pwm[3] = rc.rc_in.THR;
+
+      pwm[0] = 1200;
+      pwm[1] = 1200;
+      pwm[2] = 1200;
+      pwm[3] = 1200;
+    }
+    else{
+      pwm[0] = 900;
+      pwm[1] = 900;
+      pwm[2] = 900;
+      pwm[3] = 900;
+    } 
+    motors.update(pwm);
+  
+
+    // controller_loop();
+
+    // Serial.print("Pozyx\n");
+    // pozyx_loop();
+    // Serial.print("\n");
+    // delay(1000);
+
+
+    // Serial.print("Thermal Camera\n");
+    // thermal_loop();
+    // Serial.print("\n");
+    delay(10);
+}
